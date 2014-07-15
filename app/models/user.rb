b1e7c122a52389eb7ce7c584b86d98f7b6417978
joydeep.paul@gmail.com <joydeep.paul@gmail.com>
2014-07-15 15:55:38 +0530

    Finished sign-in sign-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 	#change to lowercase before saving the user data to database
 	before_save { self.email = email.downcase }
 
+	before_create :create_remember_token
+
 	#name should be present with maximum length of 50 chars
 	validates :name,  presence: true, length: { maximum: 50 }
 
@@ -14,4 +16,17 @@ class User < ActiveRecord::Base
 
 	has_secure_password
     validates :password, length: { minimum: 6 }
+
+    def User.new_remember_token
+    	SecureRandom.urlsafe_base64
+    end
+
+    def User.digest(token)
+    	Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+    def create_remember_token
+    	self.remember_token = User.digest(User.new_remember_token)
+    end
 end
